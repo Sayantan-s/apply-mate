ec0abac0150d45ffd17c1fc85867f271ce2aef7c
Sayantan <sayantan@hyresnap.cm>
2025-07-05 18:16:52 +0530

    feat: added changes

--- server/api/v1/jdmatch/index.post.ts
+++ server/api/v1/jdmatch/index.post.ts
@@ -3,10 +3,7 @@ import { randomBytes } from "crypto";
 import { store } from "~/shared/utils/cache";
 
 export default defineEventHandler(async (event) => {
-<<<<<<< Updated upstream
-=======
   Logging.client.logger.info(`starting jdmatch()`);
->>>>>>> Stashed changes
   const runtimeConfig = useRuntimeConfig(event);
   const API_URL = runtimeConfig.API_URL as string;
   const fileId = randomBytes(16).toString("hex");
@@ -19,37 +16,28 @@ export default defineEventHandler(async (event) => {
     fileId,
   });
 
-<<<<<<< Updated upstream
   const targetUrl = `${API_URL}/jdmatch/consumer`;
 
-=======
   const headers = {
     "x-logging-id": Logging.client.id,
   };
 
-  const targetUrl = `${API_URL}/jdmatch/consumer`;
-
   Logging.client.logger.info(`Publishing task of ${fileId}`);
->>>>>>> Stashed changes
+
   await Task.instance.publishJSON({
     url: targetUrl,
     method: "POST",
     body: info,
+    headers,
   });
-<<<<<<< Updated upstream
-=======
+
   Logging.client.logger.info(`Published task of ${fileId}`);
->>>>>>> Stashed changes
 
   await store.set(fileId, {
     status: JDMATCH_STATUS.QUEUED,
   });
 
   setResponseStatus(event, 200, "Task Created");
-
-<<<<<<< Updated upstream
-=======
   Logging.client.logger.info(`ending jdmatch()`);
->>>>>>> Stashed changes
   return { fileId };
 });
