20e2dbccd04c49a7befa898f7f53355f8cf5f8a5
Sayantan-s <sssamanta789@gmail.com>
2025-09-21 14:15:03 +0530

    ci: supabase url log

--- shared/integrations/supabase.ts
+++ shared/integrations/supabase.ts
@@ -3,4 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.DB_URI as string;
 const supabaseKey = process.env.DB_SECRET as string;
 
+console.log(`supabaseUrl, supabaseKey`, supabaseUrl, supabaseKey);
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
